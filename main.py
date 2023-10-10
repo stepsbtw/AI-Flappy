00ffca886f51c294bbd85667e58184a8f4db993c
stepsbtw <119456930+stepsbtw@users.noreply.github.com>
2023-10-10 19:15:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,28 @@ import pygame
 
 def main():
 
+
+class Bird():
+   def __init__(self,x,y):
+      self.score = 0
+      self.x = x
+      self.y = y
+      self.weights = []
+   
+   def vision(xpipe, ypipe):
+# pega o x e y do ponto
+# diminui do x e y do passaro
+# chama o think
+
+   def think(xdelta, ydelta):
+# pega os deltas e decide o output
+
+   def jump(output):
+# pula!
+      
+
+class Pipe():
+   def __init__(self,x,y,height):
+       self.height = height
+       self.x = x
+       self.y = y
