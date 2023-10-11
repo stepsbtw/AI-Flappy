e3b669634c805fe60ff538c461c967e0510071e8
stepsbtw <stepsbtw@gmail.com>
2023-10-10 23:28:46 -0300

    Master Update

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import pygame
+import os
 
-SCREEN_WIDTH = 300
+SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 800
 SCREEN_SCALE = 2
 
 WINDOW = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
 
-SPRITES_BIRD = [pygame.image.load(),pygame.image.load()]
-SPRITE_PIPE = pygame.image.load()
-SPRITE_FLOOR = pygame.image.load()
-SPRITE_BG = pygame.image.load()
+SPRITES_BIRD = [pygame.image.load(os.path.join('sprites', 'bird1.png')),pygame.image.load(os.path.join('sprites', 'bird3.png'))]
+SPRITE_PIPE = pygame.image.load(os.path.join('sprites', 'pipe.png'))
+SPRITE_FLOOR = pygame.image.load(os.path.join('sprites', 'base.png'))
+SPRITE_BG = pygame.image.load(os.path.join('sprites', 'bg.png'))
 
 def main():
-    is_running = True
-    while is_running:
-        clock.tick(60) 
-        window.fill(white) #colocar o BG aqui
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT:
-                is_running = False 
-        pygame.display.update() 
+   is_running = True
+   while is_running:
+      clock.tick(60)
+      window.fill(white) #colocar o BG aqui
+      for event in pygame.event.get(): 
+         if event.type == pygame.QUIT:
+            is_running = False 
+      pygame.display.update() 
 
-    pygame.quit()
+   pygame.quit()
 
 class Bird():
    def __init__(self,x,y):
@@ -31,30 +32,30 @@ class Bird():
       self.height = y
       self.speed = 0
       self.tick = 0
-      self.accel = 0
-      self.weights = []
+      self.accel = 1.5
+      self.weights = [0,0]
    
    def vision(self, xpipe, ypipe):
-# pega o x e y do ponto
-# diminui do x e y do passaro
-# chama o think
+      x
+      # pega o x e y do ponto
+      # diminui do x e y do passaro
+      # chama o think
+
 
    def think(self, xdelta, ydelta):
-# pega os deltas e decide o output
+      x
+      # pega os deltas e decide o output
 
    def jump(self, output):
       self.speed = -5 # valor negativo no y, boneco vai pra cima!
-# pula!
+      # pula!
 
    def move(self):
-       # SORVETAO, PARABOLA! S = So + Vo + at^2/2
-       self.tick+=1
-       movement = (self.accel*(self.tick**2))/2 + self.speed
-
-       self.y += movement
+      # SORVETAO, PARABOLA! S = So + Vo + at^2/2
+      self.tick+=1
+      movement = (self.accel*(self.tick**2))/2 + self.speed
+      self.y += movement
        
-      
-
 class Pipe():
    def __init__(self,x,y,height):
        self.height = height
