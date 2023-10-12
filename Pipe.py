a67733d08a201d468f55b26cf11972506093014b
stepsbtw <119456930+stepsbtw@users.noreply.github.com>
2023-10-12 08:48:08 -0300

    Update Pipe.py

--- Pipe.py
+++ Pipe.py
@@ -2,7 +2,7 @@ class Pipe:
    def __init__(self, x, y):
       self.x = x
       self.y = y
-      self.sprite = SPRITE_PIPE
+      self.sprite = pygame.image.load(os.path.join('sprites', 'pipe.png'))
 
    def draw(self,window):
       window.blit(self.sprite,(self.x,self.y))
