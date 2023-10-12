402288caf03c75567e81e6d9b0d33bad7885794a
stepsbtw <119456930+stepsbtw@users.noreply.github.com>
2023-10-12 20:00:26 -0300

    ta evoluindo a coisa

--- AI.py
+++ AI.py
@@ -1,6 +1,52 @@
 import pygame
 import random
 
+def eyes(self, deltas):
+    best_distance = 
+
+def brain(self, foods):
+    receptors = self.eyes()
+    if receptors == None:
+        return
+
+    outputs = [0, 0]
+
+    outputs[0] = (receptors[1] * self.inputs[0]) + (receptors[0] * self.inputs[2])
+    outputs[1] = (receptors[1] * self.inputs[1]) + (receptors[0] * self.inputs[3])
+
+     self.decision(outputs[0], outputs[1])
+
+
+class Generations:
+    def __init__(self, generation):
+        self.generation = generation
+
+    def new_gen(self, players):
+        self.generation += 1
+        mvp = self.mvp(creatures)
+        players.remove_all()
+        if mvp.score > 0:
+            for i in range(num_players):
+                mvp_weights = mutate(mvp.weights)         
+                players.append(Player(weights = mvp_weights))
+        else:
+            mvp_weights = mvp.weights
+            creatures.append(Player(weights = mvp_weights))
+            for i in range(num_players-1):
+                players.append(Player())
+
+        players.remove_all()
+
+    def mvp(players):
+        mvp = players[0]
+        best = 0
+        for player in players:
+            if players.score > best:
+                best = player.best
+                mvp = player
+        return mvp
+
+
 if not have_brain:
     self.weights = [random.randInt(-100,100),random.randInt(-100,100),random.randInt(-100,100),random.randInt(-100,100)] 
 self.score = 0 
@@ -27,6 +73,3 @@ def mutation(inputs):
          inputs[i] = random.choice(list_mutation) 
   
      return inputs
-
-
-# falta adicionar o sistema de peso
