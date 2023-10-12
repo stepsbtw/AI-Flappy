e2f41c8d28a0e7a38e7f8730f3ffad521950f59a
stepsbtw <119456930+stepsbtw@users.noreply.github.com>
2023-10-12 09:26:46 -0300

    Update Pipe.py

--- Pipe.py
+++ Pipe.py
@@ -6,9 +6,11 @@ class Pipe:
       self.x = x
       self.y = y
       self.sprite = pygame.image.load(os.path.join('sprites', 'pipe.png'))
+      self.sprite_invert = pygame.transform.flip(self.sprite,False,True)
 
    def draw(self,window):
       window.blit(self.sprite,(self.x,self.y))
+      window.blit(self.sprite_invert,(self.x,self.y))
       #window.blit(self.sprite,(self.x,self.y))
 
 #printar o inverso, um cano sempre tem o seu cano reverso.
