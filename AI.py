e358990458a1305ec921fb96a8e1044f432aa144
stepsbtw <119456930+stepsbtw@users.noreply.github.com>
2023-10-12 18:37:12 -0300

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,1 +1,15 @@
 import pygame
+
+self.weights = [0, 0] 
+self.score = 0 
+
+def vision(self, pipe): 
+    deltax = self.x - pipe.x
+    deltay = self.y - (pipe.y + pipe.y_invert)/2
+    think(deltax, deltay)
+  
+def think(self, deltax, deltay):
+    if deltax < deltay:
+        self.jump()
+
+# falta adicionar o sistema de peso
