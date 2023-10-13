e1c9e5b82cc949c0a5d0a4f596c1f772d759349c
stepsbtw <119456930+stepsbtw@users.noreply.github.com>
2023-10-13 18:43:51 -0300

    Update Evolution.py

--- Evolution.py
+++ Evolution.py
@@ -9,17 +9,20 @@ class Evolution:
     def new_gen(self, old_players):
         new_players = []
         self.generation += 1
+        print(f'geracao: {self.generation}')
         if not old_players:
-            print(f'geracao: {self.generation}')
             for i in range(100):
-                    new_players.append(Bird([]))
+                    new_players.append(Bird([random.randint(-1000,1000),
+                                               random.randint(-1000,1000),
+                                               random.randint(-1000,1000),
+                                               random.randint(-1000,1000)]))
                     self.generation += 1
                     return new_players
         mvp = self.battle_royale(old_players)
         if mvp.ia_score > 0: # caso haja um melhor, selecao natural!
             print('selecao natural!')
             #players.clear()
-            for i in range(len(old_players)):
+            for i in range(100):
                 weights = self.mutate(mvp.weights)      
                 new_players.append(Bird(weights))
             old_players.clear()
@@ -27,17 +30,17 @@ class Evolution:
         else:      # caso todos sejam podres, aniquilacao, cataclisma, eclipse, apocalipse
             print('nao teve selecao')
             #old_players.clear()
-            for i in range(len(old_players)):
-                new_players.append(Bird([mvp.weights]))
+            for i in range(100):
+                new_players.append(Bird(self.mutate(mvp.weights)))
             old_players.clear()
             return new_players
 
     #quando achar o melhor, e passar pra frente, variar!
     def mutate(self,weights): # variacao genetica dos pesos, pegar e somar uma constante pequena!
-        for i in range(len(weights)):
+        for i in range(4):
             weight = random.randint(0,100) # peso do peso
             mutations = (weights[i] + weight, weights[i] - weight)
-            weights.append(random.choice(mutations))
+            weights[i] = random.choice(mutations)
         return weights
 
     def battle_royale(self,players):
@@ -45,5 +48,6 @@ class Evolution:
         mvp_score = 0
         for i,player in enumerate(players):
             if players[i].ia_score > mvp_score:
-                mvp_score = player.ia_score
+                mvp_score = players[i].ia_score
+        print(f'record : {mvp_score}')
         return mvp
