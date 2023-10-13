7d5b471e4191a52099873480c553415c9023c07b
stepsbtw <119456930+stepsbtw@users.noreply.github.com>
2023-10-13 18:41:44 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from Pipe import Pipe
 from Base import Base
 from Evolution import Evolution
 from Bird import Bird
+import random
 
 
 def main():
@@ -17,11 +18,12 @@ def main():
    players = []
    dead_players = []
    DARWIN = Evolution(0)
-   CURRENT_PIPE = pipes[0]
+   CURRENT_PIPE_NUM = 0
    MVP_SCORE = 0
    while is_running:
       clock.tick(60)
       window.fill((255, 255, 255))
+      CURRENT_PIPE = pipes[CURRENT_PIPE_NUM]
       CURRENT_GENERATION = DARWIN.generation
       for event in pygame.event.get():
          if event.type == pygame.QUIT:
@@ -30,22 +32,23 @@ def main():
          MVP_SCORE = 0
          pipes = [Pipe()]
          players = DARWIN.new_gen(dead_players)
-      for i,player in enumerate(players):
-            if player.DEAD:
-               dead_players.append(player)
-               players.pop(i)
-            player.brain(CURRENT_PIPE)
       base.move()
-      if(check_score(pipes,players,dead_players)):
-         MVP_SCORE+=1
-         CURRENT_PIPE = pipes[MVP_SCORE] # isso ta dando problema.
+      for i,player in enumerate(players):
+         if player.DEAD:
+            dead_players.append(player)
+            players.pop(i)
+         player.move()
+         player.brain(CURRENT_PIPE)
+      plus , CURRENT_PIPE = check_score(pipes,players,dead_players,CURRENT_PIPE_NUM,CURRENT_PIPE)
+      if plus:
+         MVP_SCORE+=1# isso ta dando problema.
       draw_all(window,players,pipes,base,MVP_SCORE,CURRENT_GENERATION)
       pygame.display.update()
 
    pygame.quit()
 
 # ta faltando um pipe.pop
-def check_score(pipes,players,dead_players): # a cada cano que some da tela, um novo é gerado.
+def check_score(pipes,players,dead_players,CURRENT_PIPE_NUM,CURRENT_PIPE): # a cada cano que some da tela, um novo é gerado.
    add_pipe = False # isso tbm ta dando merda. add_pipe = score
    remove_pipes = []
    for pipe in pipes:
@@ -55,19 +58,24 @@ def check_score(pipes,players,dead_players): # a cada cano que some da tela, um 
             player.DEAD = True
             dead_players.append(player)
             players.pop(i)
-         if not pipe.score and player.x > pipe.x: # caso ainda nao tenha sido feito ponto e o jogador passou, ponto!
-            pipe.score = True
-            add_pipe = True # se foi ponto, adicionar um cano
-            player.ia_score +=1
+         if not pipe.score:
+               if player.x < pipe.x:
+                  player.ia_score += ((pipe.y_invert-pipe.DISTANCE/2)- player.y)/100
+               if player.x > pipe.x: # caso ainda nao tenha sido feito ponto e o jogador passou, ponto!
+                  pipe.score = True
+                  add_pipe = True # se foi ponto, adicionar um cano
+                  player.ia_score *= player.ia_score
+                  CURRENT_PIPE_NUM += 1
       if pipe.x + pipe.sprite_width < 0: # se o pipe saiu, entra na fila de remocao.
          remove_pipes.append(pipe)
    if add_pipe:
-      pipes.append(Pipe()) # apenas adicionar o novo cano fora do loop.
+      pipes.append(Pipe()) 
+      CURRENT_PIPE = pipes[CURRENT_PIPE_NUM]
+   # apenas adicionar o novo cano fora do loop.
    for pipe in remove_pipes: # remover os que ja sairam da tela.
       remove_pipes.remove(pipe)
-   return add_pipe # se adicionei um pipe, prox pipe.
+   return add_pipe,CURRENT_PIPE # se adicionei um pipe, prox pipe.
       
-
 def draw_all(window,birds,pipes,base,mvp_score,generation):
    window.blit(Globals.SPRITE_BG,(0,0))
    for pipe in pipes:
